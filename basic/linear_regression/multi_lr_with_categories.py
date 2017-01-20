abd3dba739dfd17ad83afa318bf753f8ae105c6e
Zhou <theodoruszq@gmail.com>
2017-01-20 11:15:53 +0800

    complete categories linear regression

---
+++ basic/linear_regression/multi_lr_with_categories.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Multiple regression using python.
+"""
+
+from numpy import genfromtxt
+import numpy as np
+from sklearn import linear_model
+
+dataPath = r'./multi_lr_with_categories.csv'
+deliveryData = genfromtxt(dataPath, delimiter = ',')
+
+print (">>> csv data\n", deliveryData)
+
+#[1:, :-1]中的 1 表示是忽略第 0 行, 从 1 到 last;
+#后面的 :-1 是表示从 0 到 last-1 列
+X = deliveryData[1:, :-1]
+Y = deliveryData[1:, -1]
+
+print (">>> X\n", X)
+print (">>> Y\n", Y)
+
+regr = linear_model.LinearRegression()
+regr.fit(X, Y)
+
+#coef is the cofficients not including the intercept
+print (">>> cofficients: \n", regr.coef_)
+print (">>> intercept: \n", regr.intercept_)
+
+exit()
+
+
+xPred = np.array([[102, 6]])
+yPred = regr.predict(xPred)
+print (">>> predicted y: \n", yPred)
+
